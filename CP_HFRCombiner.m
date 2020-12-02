6914ffdde9acfbdf886966ad597a1ec8d1e4b711
LorenzoCorgnati <lorenzo.corgnati@sp.ismar.cnr.it>
2020-12-02 14:24:07 +0100

    Bugs fixed.

--- CP_HFRCombiner.m
+++ CP_HFRCombiner.m
@@ -264,6 +264,7 @@ try
                 
                 % Convert the radial files into netCDF according to the European standard data model
                 for ruv_idx=1:length(toBeCombinedRadialIndices)
+                    HFRC_err = 0;
                     station_tbUpdateFlag = 0;
                     toBeCombinedStationIndexC = strfind(stationData(:,STstation_idIndex), toBeCombinedRadials_data{toBeCombinedRadialIndices(ruv_idx),RIstation_idIndex});
                     toBeCombinedStationIndex = find(not(cellfun('isempty', toBeCombinedStationIndexC)));
@@ -287,6 +288,7 @@ try
                         contrSitesIndices(ruv_idx) = toBeCombinedStationIndex;
                     catch err
                         display(['[' datestr(now) '] - - ERROR in ' mfilename ' -> ' err.message]);
+                        contrSitesIndices(ruv_idx) = toBeCombinedStationIndex;
                         HFRC_err = 2;
                     end
                     
@@ -339,7 +341,7 @@ try
                 end
                 
                 % Combine the Codar radial files into total
-                if((size(radFiles,2)>1) && (HFRC_err~=2))
+                if((size(radFiles,2)>1) && (HFRC_err~=1))
                     % Check if all the radials have the same extension
                     for ext_idx=1:length(toBeCombinedRadialIndices)
                         extensions{ext_idx} = toBeCombinedRadials_data{toBeCombinedRadialIndices(ext_idx),extensionIndex};
